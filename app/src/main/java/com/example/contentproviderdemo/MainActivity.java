8c39e59a1e21da1deb502fbfdf608e699084b4f6
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2022-11-26 12:59:51 +0300

    get phone contacts

--- app/src/main/java/com/example/contentproviderdemo/MainActivity.java
+++ app/src/main/java/com/example/contentproviderdemo/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.contentproviderdemo;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.example.contentproviderdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view -> {
+
+        });
     }
+
+    private void getPhoneContacts() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CONTACTS
+        ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    0
+            );
+        }
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(
+                uri,
+                null,
+                null,
+                null,
+                null
+        );
+
+        Log.i("CONTACT_PROVIDER_DEMO", "TOTAL # of Contacts : " + cursor.getCount());
+        if (cursor.getCount() > 0){
+            while (cursor.moveToNext()){
+                String contactName = cursor.getCount() > 0 ?cursor.getString(
+                        cursor.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                        )
+                ) : "empty";
+            }
+        }
+
+    }
+
 }
